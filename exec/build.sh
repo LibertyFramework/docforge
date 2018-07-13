f0fee1b4a47fad0a8230ee90f11aa52ff20209bf
Francesco Bianco <info.francescobianco@gmail.com>
2018-07-13 17:47:31 +0200

    Version 0.0.2

--- exec/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $1 > /dev/null 2>&1 & cd $1
-echo '{"directory": "vendor"}' > .bowerrc
-bower install uikit angular angular-ui-router
---
+++ src/Server.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * File description.
+ *
+ * PHP version 5
+ *
+ * @category -
+ *
+ * @author    -
+ * @copyright -
+ * @license   -
+ */
+
+namespace Javanile\Elegy;
+
+class Server
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function run()
+    {
+        echo "Server";
+
+
+    }
+}
